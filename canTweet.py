4a5cb877696ad0c8190f2c8f42f00d94b74068d4
bison-- <mike.bison42@googlemail.com>
2017-12-21 21:16:52 +0100

    now with objects

--- canTweet.py
+++ canTweet.py
@@ -1,3 +1,32 @@
+import json
 
-def can_tweet(last_revid):
-    return True
+class canTweet(object):
+
+    def __init__(self):
+        self.last_revid = 0
+        self.memoryFile = 'last_revid.json'
+        try:
+            with open(self.memoryFile) as fHandle:
+                self.last_revid = json.load(fHandle)
+        except Exception as ex:
+            # TODO: empty file or no file at all, get the propper error
+            pass
+
+    def can_tweet(self, revid):
+        return revid > self.last_revid
+
+
+    def set_last_revid(self, revid):
+        self.last_revid = revid
+
+    def save(self):
+        with open(self.memoryFile, 'w') as fHandle:
+            json.dump(self.last_revid, fHandle)
+
+
+if __name__ == '__main__':
+    cTweet = canTweet()
+    if cTweet.can_tweet(124):
+        print('OK')
+    cTweet.set_last_revid(124)
+    cTweet.save()
--- startScript.py
+++ startScript.py
@@ -1,7 +1,12 @@
 import readwiki
 import canTweet
 
+cTweet = canTweet.canTweet()
+
 for message, last_revid in readwiki.get_changes():
-    if canTweet.can_tweet(last_revid):
+    if cTweet.can_tweet(last_revid):
         print('you can tweet this:', last_revid)
+        cTweet.set_last_revid(last_revid)
     print(message)
+
+cTweet.save()
