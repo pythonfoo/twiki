628f441c0274db3eb64fd4b885c8f0e5bc39e7f3
bison-- <mike.bison42@googlemail.com>
2018-03-15 19:40:28 +0100

    start script changes
    (already obsolete)

--- startScript.py
+++ startScript.py
@@ -3,10 +3,23 @@ import canTweet
 
 cTweet = canTweet.canTweet()
 
+maxEntries = 10
+entriesToCheck = {}
+
+# we need the entries in oldest to newest order, get_changes only delivers in newest to oldest
+msgCount = 0
 for message, last_revid in readwiki.get_changes():
-    if cTweet.can_tweet(last_revid):
-        print('you can tweet this:', last_revid)
-        cTweet.set_last_revid(last_revid)
-    print(message)
+    entriesToCheck[last_revid] = message
+    msgCount += 1
+    if msgCount >= maxEntries:
+        break
+
+revIds = sorted(entriesToCheck.keys())
+for revid in revIds:
+    message = entriesToCheck[revid]
+    if cTweet.can_tweet(revid):
+        print('you can tweet this:', revid)
+        print(message)
+        cTweet.set_last_revid(revid)
 
 cTweet.save()
